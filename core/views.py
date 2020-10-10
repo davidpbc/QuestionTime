d799322e9f149498b763a4cd35fd561e5a9d7ec4
David Padilla <david.pabc@gmail.com>
2020-10-10 09:00:19 -0500

    Create Question Time repo

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from typing import List
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
+
+
+class IndexTemplateView(LoginRequiredMixin, TemplateView):
+
+    def get_template_names(self) -> List[str]:
+        template_name = 'index.html'
+        return [template_name]
